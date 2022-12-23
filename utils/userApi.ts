ba3adf4ac4cd7c3e51a4fca6b2a679266c4c23d8
TheNeonLeon <leon.listo@hotmail.com>
2022-12-23 10:13:24 +0100

    add user to db test

--- utils/userApi.ts
+++ utils/userApi.ts
@@ -1,9 +1,32 @@
-import  db  from '../firebase/firebaseConfig'
-import { collection, doc, getDocs } from "firebase/firestore"; 
+import db from "../firebase/firebaseConfig";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
-export async function getUsers(db:any) {
-    const userCollection = collection(db, 'users');
-    const snapshot = await getDocs(userCollection);
-    const list = snapshot.docs.map(doc => doc.data());
-    return list;
+export const getUsers = async (db:any) => {
+  const userCollection = collection(db, "users");
+  const snapshot = await getDocs(userCollection);
+  const list = snapshot.docs.map((doc) => doc.data());
+  return list;
+};
+
+export const addUser = async () => {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+     userInfo: {
+      firstName: "Billy",
+      lastName: "Bob",
+      email: "bob@mail.com",
+      country: "Sweden",
+      streetAdress: "pepparkaksgatan 2",
+      city: "Gothenburg",
+      postalCode: 43345,
+      company: {
+        companyName: "Telia",
+        isActive: true
+      }
+     }
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
   }
+};
