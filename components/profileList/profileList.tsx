5932494205be991d83494350bd7aa3f7cdac34ed
TheNeonLeon <leon.listo@hotmail.com>
2023-01-12 00:58:50 +0100

    FEAT: add some more logic and styling

--- components/profileList/profileList.tsx
+++ components/profileList/profileList.tsx
@@ -1,9 +1,17 @@
-import { collection, doc, getDoc, getDocs, setDoc, updateDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import db from "../../firebase/firebaseConfig";
-import {  getCompanies, getUsers } from "../../utils/userApi";
+import { getCompanies, getUsers } from "../../utils/userApi";
+import { CompanyProps } from "../company/type";
 import { FormDataProps } from "../form/type";
 
 export default function Profile() {
@@ -21,24 +29,27 @@ export default function Profile() {
 
   const router = useRouter();
 
-  const updateUser = async (id:string, company:any, status:boolean) => {
+  const updateUser = async (id: string, company: any, status: boolean) => {
     const userDoc = doc(db, "users", id);
-    setStatus(true);
-    const newField = {companyInfo: 
-      {
+    const newField = {
+      companyInfo: {
         companyName: company,
-        isActive: !status
-      }
+        isActive: !status,
+      },
+    };
+    if(companyName.length > 0){
+      setStatus(true);
+      await updateDoc(userDoc, newField);
+      router.reload();
+    }else{
+      alert("Error")
     }
-    await updateDoc(userDoc, newField);
-    router.reload();
-  }
+  };
 
   useEffect(() => {
     const getUserData = async () => {
       const fetchUserData: Promise<any> = getUsers(db);
       setUsers(await fetchUserData);
-      
     };
     const getCompanyData = async () => {
       const data: Promise<any> = getCompanies(db);
@@ -53,53 +64,51 @@ export default function Profile() {
       <h1 className="text-4xl font-extrabold dark:text-white">Profiles</h1>
       <ul>
         <>
-          {users.map((data: any) => {
+          {users.map((data: any, key) => {
             return (
               <>
                 {data.companyInfo.isActive == false ? (
-                    <div>
+                  <div>
                     <div className="flex border-2">
-                     
                       <label
-                      
                         htmlFor="first-name"
                         className="block text-sm font-medium text-gray-700"
-                      >
-          
-                      </label>
+                      ></label>
 
-                      <p>
+                      <p key={key}>
                         {data.userInfo.firstName} is not active in any company
                       </p>
-                      
                     </div>
-                    {company.map((companyData: any) => {
-
-                      return (
-                        <>
-                          <div className="flex">
-                          <label
-                        htmlFor="country"
-                        className="block text-sm font-medium text-gray-700"
+                    <>
+                      <div className="flex">
+                        <label
+                          htmlFor="country"
+                          className="block text-sm font-medium text-gray-700"
+                        >
+                          Company
+                        </label>
+                        <select
+                          onChange={(e) => {
+                            setCompanyName(e.target.value);
+                          }}
+                          defaultValue="Choose"
+                          className="mt-1 w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        >
+                          <option value="Choose" disabled>
+                            Not Selectable
+                          </option>
+                          {company.map((companyData: CompanyProps, key) => {
+                            return <option key={key}>{companyData.companyName}</option>
+                          })}
+                        </select>
+                      </div>
+                      <button
+                        onClick={() => updateUser(data.id, companyName, status)}
                       >
-                        Company
-                      </label>
-                      <select
-                        id="company"
-                        onChange={(e) => {setCompanyName(e.target.value)}}
-                        autoComplete="company-name"
-                        className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      >
-                        <option >choose:</option>
-                        <option>{companyData.companyName}</option>
-                      </select>
-                          </div>
-                          <button onClick={()=>updateUser(data.id,companyName,status)}>Add to company</button>
-
-                        </>
-                      );
-                    })}
-                 </div>
+                        Add to company
+                      </button>
+                    </>
+                  </div>
                 ) : (
                   ""
                 )}
